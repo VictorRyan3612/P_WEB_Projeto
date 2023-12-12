f8d5397504342473a9ddfcffe133b3a2165f952b
Victor Ryan <vitorsilva3612@gmail.com>
2023-12-12 18:40:42 -0300

    Tela detalhe filme parte 1

---
+++ src/pages/[imdId].js
@@ -0,0 +1,29 @@
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
+
+
+export default function TheMovie(){
+   const {imdId} = useRouter().query
+   const {data, error} = useSWR(`http://localhost:3000/api/filme/${imdId}`, async (u) => {
+      const res = await fetch(u)
+      const json = await res.json();
+      return json;
+   })
+
+   if (error) return <div>Erro na requisição/resposta </div>
+   if (!data){
+      return <div>Carregando...</div>
+   } 
+   if (data.Error) return <div>Erro</div>
+   console.log(data)
+   return (
+      <div>
+         <center>
+            <br />
+               <h2>Título: {data.title}</h2>
+               <h3>Ano: {data.year}</h3>
+               <img src={data.poster} width="200" alt={`Poster for ${data.title}`} />
+         </center>
+      </div>
+   )
+}
