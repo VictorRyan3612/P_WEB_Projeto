219290531bd386fe063e489472a862be87535153
Victor Ryan <vitorsilva3612@gmail.com>
2023-12-12 17:28:37 -0300

    Listagem geral frontend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,40 @@
-export default function Home(){
+import React, { useEffect, useState } from 'react';
+
+const Index = () => {
+   const [dataMovies, setDataMovies] = useState([]);
+
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+         const response = await fetch('http://localhost:3000/api/filmes');
+         const data = await response.json();
+         
+         if (data.result) {
+            setDataMovies(data.result);
+         }
+         } catch (error) {
+         console.error('Erro ao buscar filmes:', error);
+         }
+      };
+
+      fetchData();
+   }, []);
+
    return (
       <div>
-         <p>
-            Hello World
-         </p>
-         <a href="/r15"> 
-            Movies 3
-         </a>
+         <center>
+         {/* Validar se não é vazio */}
+         {dataMovies && dataMovies.map((m) => (
+            <div key={m.imdId}>
+               <br />
+               <h2>Título: {m.title}</h2>
+               <h3>Ano: {m.year}</h3>
+               <img src={m.poster} width="200" alt={`Poster for ${m.title}`} />
+            </div>
+         ))}
+         </center>
       </div>
-      
-   )
-}
+   );
+};
+
+export default Index;
